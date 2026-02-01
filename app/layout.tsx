1c140018ddbd00050faa6a0d0e42c68e0d7318e9
vaibwork <vaibwork2021@gmail.com>
2026-02-01 21:07:36 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'SS CREATION | Corporate Interior Fit-Out',
+  description: 'End-to-end corporate interior fit-out contractor based in Kolkata'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
