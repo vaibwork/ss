6fc9820687e0b4d9fa1cba566ad88059bdfc9f75
vaibwork <vaibwork2021@gmail.com>
2026-02-01 21:29:32 +0530

    Update and rename layout.tsx to layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'SS CREATION | Corporate Interior Fit-Out',
+  description: 'Corporate turnkey interior contractor based in Kolkata',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
